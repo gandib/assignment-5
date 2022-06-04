3ffec407b9530132435e38bea1fd9dfd8d32066a
Gandib Roy <gandibroy11@gmail.com>
2022-06-04 22:44:54 +0600

    sixth commit

--- js/app.js
+++ js/app.js
@@ -1,9 +1,25 @@
 function getInput(inputName) {
     const input = document.getElementById(inputName);
     const inputText = input.value;
+    const stringHandling = document.getElementById('string-handling');
+    const numberHandling = document.getElementById('negative-number-handling');
+    if (!inputText) {
+        stringHandling.style.display = 'block';
+        numberHandling.style.display = 'none';
+        console.log(stringHandling);
+        return stringHandling;
+    }
+    if (inputText < 0) {
+        stringHandling.style.display = 'none';
+        numberHandling.style.display = 'block';
+        console.log(numberHandling);
+        return numberHandling;
+    }
 
     if (inputText >= 0) {
         const inputValue = parseFloat(inputText);
+        // stringHandling.style.display = 'none';
+        // numberHandling.style.display = 'none';
         return inputValue;
     }
     else {
@@ -16,13 +32,15 @@ function totalExpens() {
     const rentInput = getInput('rent-input');
     const clothesInput = getInput('clothes-input');
     const numberHandling = document.getElementById('negative-number-handling');
+    const stringHandling = document.getElementById('string-handling');
     if (foodInput >= 0 && rentInput >= 0 && clothesInput >= 0) {
         const expenses = foodInput + rentInput + clothesInput;
         numberHandling.style.display = 'none';
+        stringHandling.style.display = 'none';
         return expenses;
     }
     else {
-        numberHandling.style.display = 'block';
+        // numberHandling.style.display = 'block';
     }
 }
 
@@ -30,6 +48,20 @@ function getBalance() {
     const incomeInput = getInput('income-input');
     const expensesTotalAmount = totalExpens();
     const errorLowIncome = document.getElementById('more-expense');
+    const stringHandling = document.getElementById('string-handling');
+    const numberHandling = document.getElementById('negative-number-handling');
+    if (!incomeInput) {
+        numberHandling.style.display = 'none';
+        stringHandling.style.display = 'block';
+        console.log(stringHandling);
+        return stringHandling;
+    }
+    // if (incomeInput < 0) {
+    //     numberHandling.style.display = 'block';
+    //     stringHandling.style.display = 'none';
+    //     console.log(numberHandling);
+    //     return numberHandling;
+    // }
     if (incomeInput > expensesTotalAmount) {
         const newBalance = incomeInput - expensesTotalAmount;
         errorLowIncome.style.display = 'none';
@@ -63,7 +95,12 @@ document.getElementById('save-btn').addEventListener('click', function () {
     const balanceAmount = getBalance();
     const errorSaving = document.getElementById('more-saving');
     let savingAmount = document.getElementById('saving-amount');
+    const savingStringHandling = document.getElementById('error-saving');
     const totalSaveAmount = incomeInput / 100 * saveInput;
+    if (!totalSaveAmount) {
+        savingStringHandling.style.display = 'block';
+        return savingStringHandling;
+    }
     if (balanceAmount >= totalSaveAmount) {
         savingAmount.innerText = totalSaveAmount;
         errorSaving.style.display = 'none';
